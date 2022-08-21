a12d49a1f7e0a4a834724ed1930639106dfef571
khm9990503 <khm9990503@gmail.com>
2022-08-22 08:46:01 +0900

    오전에 수제 마침

--- Book1/src/ch04/P119.java
+++ Book1/src/ch04/P119.java
@@ -1,5 +1,16 @@
 package ch04;
 
 public class P119 {
+	public static void main(String[] args) {
+		int total = 0;
+		int num;
+		
+		for(num=1;num<=100;num++) {
+			if(num%2==0)
+				continue; // num 값이 짝수인 경우, 뒤 수행을 생략
+			total+=num;
+		}
+		System.out.println("1부터 100까지의 홀수의 합은 : " + total + "입니다.");
+	}
 
 }
