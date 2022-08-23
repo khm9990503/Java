d1076148630ccf0d29c60c4f7a4344569525184b
khm9990503 <khm9990503@gmail.com>
2022-08-23 13:57:59 +0900

    백준

--- Backjoon/src/step02/_05.java
+++ Backjoon/src/step02/_05.java
@@ -1,5 +1,33 @@
 package step02;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/23
+ * 이름 : 구홍모
+ * 내용 : 2단계 5번 - 알람 시계
+ */
 public class _05 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if(M>=45 && H>=1) {
+			System.out.println(H+" "+(M-45));
+			
+		}else if(M<45 && H>=1){
+			System.out.println((H-1)+" "+(60+(M-45)));
+			
+		}else if(M<45 && H==0){
+			System.out.println("23 "+(60+(M-45)));
+		
+		}else if(M>=45 && H==0){
+			System.out.println("0 "+(M-45));
+			
+		}
+		sc.close();
+	}
 
 }
--- Backjoon/src/step02/_06.java
+++ Backjoon/src/step02/_06.java
@@ -1,5 +1,28 @@
 package step02;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/23
+ * 이름 : 구홍모
+ * 내용 : 2단계 6번 - 오븐 시계
+ */
 public class _06 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if (((M+C)/60)>=1) {
+			System.out.println((H+((M+C)/60))+" "+((M+C)%60));
+			
+		}else if(((M+C)/60)==0) {
+			System.out.println(H+" "+((M+C)%60));
+			
+		}
+		sc.close();		
+	}
 
 }
