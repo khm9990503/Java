6ca053f23e000aceca0ae1aadd8c33ebb27ee077
khm9990503 <khm9990503@gmail.com>
2022-08-24 13:55:40 +0900

    백준 갱신

--- Backjoon/src/step03/_03.java
+++ Backjoon/src/step03/_03.java
@@ -1,5 +1,25 @@
 package step03;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/24
+ * 이름 : 구홍모
+ * 내용 : 3단계 3번 - 합
+ */
 public class _03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int tot=0;
+		
+		for (int i=1; i<=n ; i++) {
+		
+			tot += i;
+			 
+		}
+		System.out.println(tot);
+		sc.close();
+	}
 
 }
