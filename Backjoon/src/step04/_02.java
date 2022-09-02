418c96dc3d1a8a84d295c60c7a1750725ef9b4d1
khm9990503 <khm9990503@gmail.com>
2022-09-02 10:52:57 +0900

    백준

--- Backjoon/src/step04/_02.java
+++ Backjoon/src/step04/_02.java
@@ -12,39 +12,18 @@ public class _02 {
 		Scanner sc = new Scanner (System.in);
 		int arr[] = new int[9];
 		int max=arr[0];
+		int loc=0;
 		for(int i=0; i<9; i++) {
-			
 			int x = sc.nextInt();
 			arr[i]=x;
 			if(max<arr[i]) {
 				max=arr[i];
+				loc=i;
 			}
 		}
-		int start = 0;
-		int end   = arr.length -1;
-		int loc   = 0;
-		boolean state = false;
-		
-		while(start <= end) {
-			
-			int mid = (start + end) / 2;
-			
-			if(arr[mid]>max) {
-				end = mid - 1;
-			}else if(arr[mid]<max) {
-				start = mid + 1;
-			}else {
-				loc = mid;
-				state = true;
-				break;
-			}
-		}
-		
-		
-		
 		sc.close();
 		System.out.println(max);
 		System.out.println(loc+1);
 	}
-
 }
+
