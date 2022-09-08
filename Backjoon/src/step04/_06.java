c0f98c3385132f007f8f791453172faf704d5c9e
khm9990503 <khm9990503@gmail.com>
2022-09-08 11:42:26 +0900

    오전백준

--- Backjoon/src/step04/_06.java
+++ Backjoon/src/step04/_06.java
@@ -1,5 +1,18 @@
 package step04;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/09/08
+ * 이름 : 구홍모
+ * 내용 : 4단계 6번 - 평균은 넘겠지
+ */
 public class _06 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int N = sc.nextInt();
+		
+	}
 
 }
