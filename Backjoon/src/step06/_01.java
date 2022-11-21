6931a2aa06782e23a2bc2e2652cd1dd477897c8a
khm9990503 <khm9990503@gmail.com>
2022-11-21 12:48:06 +0900

    11/21

--- Backjoon/src/step06/_01.java
+++ Backjoon/src/step06/_01.java
@@ -1,6 +1,10 @@
 package step06;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /*
  * 날짜 : 2022/09/15
@@ -8,10 +12,18 @@ import java.util.Scanner;
  * 내용 : 6단계 1번 - 아스키 코드
  */
 public class _01 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		String s = br.readLine();
+		int i = Integer.parseInt(s);
 		
+		bw.write(s);
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
